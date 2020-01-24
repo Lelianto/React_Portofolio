4054a0cf261a29814803a3afca9ac1368adf9db8
Lelianto Eko Pradana <lian@alterra.id>
2020-01-24 10:48:16 +0700

    move all file

---
+++ build/precache-manifest.f18c6e7f1c7638c2e536829ea0923389.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20f15a23ed0634cc28a17d6dcdec245a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4c6b771573ec1ae6953",
+    "url": "/static/css/main.b8d8f69e.chunk.css"
+  },
+  {
+    "revision": "8ca0431d96bc43365823",
+    "url": "/static/js/2.2b9041a8.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.2b9041a8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f4c6b771573ec1ae6953",
+    "url": "/static/js/main.79ccc39a.chunk.js"
+  },
+  {
+    "revision": "27ce05b47bf38b4da3e1",
+    "url": "/static/js/runtime-main.88017583.js"
+  },
+  {
+    "revision": "4cd8e9a792278376e920e094c069b6c8",
+    "url": "/static/media/arista2.0.4cd8e9a7.woff"
+  },
+  {
+    "revision": "8c15e6ad8fc16fb3f1f8fd507cfc62a1",
+    "url": "/static/media/arista2.0.8c15e6ad.woff2"
+  },
+  {
+    "revision": "707e40335e2104d8d0c0d48e495882a4",
+    "url": "/static/media/arista2.0alternate.707e4033.woff"
+  },
+  {
+    "revision": "768898fe9c8a23514bc8eee7bd5da897",
+    "url": "/static/media/arista2.0alternate.768898fe.woff2"
+  },
+  {
+    "revision": "e8b47ab415f9cb4f4c612526fc5634c2",
+    "url": "/static/media/books.e8b47ab4.png"
+  },
+  {
+    "revision": "c22bda41b8844078c062d53a603383eb",
+    "url": "/static/media/bug-logo.c22bda41.png"
+  },
+  {
+    "revision": "869394f090d7031c3a9c6f5d4b9a93be",
+    "url": "/static/media/buy.869394f0.png"
+  },
+  {
+    "revision": "ef47b5a250be440e15767e8d324f11ea",
+    "url": "/static/media/cart.ef47b5a2.webp"
+  },
+  {
+    "revision": "ad0ef673363f83786f2bdcd5db26b6a1",
+    "url": "/static/media/employee.ad0ef673.png"
+  },
+  {
+    "revision": "8e01d052738d6e27c0d923779b01b559",
+    "url": "/static/media/empty.8e01d052.jpg"
+  },
+  {
+    "revision": "fd763635ec84b23db3cb932fa5d4befc",
+    "url": "/static/media/exit.fd763635.png"
+  },
+  {
+    "revision": "107b1afb46d59633b15f75b3fed8901b",
+    "url": "/static/media/fotobank.107b1afb.jpg"
+  },
+  {
+    "revision": "3f8d18d84afb65cf6c746b83eaddb3fb",
+    "url": "/static/media/promo1.3f8d18d8.jpg"
+  },
+  {
+    "revision": "aa74f58e36e1fe869fb0eff1c9db9a1e",
+    "url": "/static/media/promo2.aa74f58e.jpg"
+  },
+  {
+    "revision": "13a0c36aef057fa4c3e55c67a4ac29f9",
+    "url": "/static/media/promo3.13a0c36a.jpg"
+  },
+  {
+    "revision": "bdcb8ec26fe8c0a43c2371c07fbecded",
+    "url": "/static/media/promo4.bdcb8ec2.jpg"
+  },
+  {
+    "revision": "3f8d18d84afb65cf6c746b83eaddb3fb",
+    "url": "/static/media/promo5.3f8d18d8.jpg"
+  },
+  {
+    "revision": "303c029454ae50c2853f0aa0d7763cb0",
+    "url": "/static/media/supermarket.303c0294.png"
+  }
+]);
